be7dabb6789a4d075ea732af6401aea7edc1717f
sophassamuel <80958670+sophassamuel@users.noreply.github.com>
2022-09-19 12:33:52 +0530

    Update javascript.js

--- WT_exp_5/javascript.js
+++ WT_exp_5/javascript.js
@@ -70,7 +70,7 @@ function flip() {
     var tail = document.getElementById("num_tails");
     var h = parseInt(head.innerHTML);
     var t = parseInt(tail.innerHTML);
-    var lst = ["Assets\head.jpeg", "Assets\tail.jpeg"];
+    var lst = ["head.jpeg", "tail.jpeg"];
     var s = Math.floor(Math.random()*2);
     if (lst[s]==lst[0]) {
         head.innerHTML = h+1;
